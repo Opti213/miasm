79fe8343e961082fe4f0cee3c91117e9a2419517
MGorbachev <opti213@mail.ru>
2019-12-03 22:55:55 +0300

    parce 'not'

--- app/utility.py
+++ app/utility.py
@@ -7,14 +7,22 @@ from app.commands import Command
 def get_command(line: str) -> Command:
     return Command(line.split()[0])
 
+
 def get_args(line: str) -> List[str]:
-    return line.replace(",", "").split()[1:]
+    return line.split(" ", 1)[1].split(", ")
+
 
 def get_arg(mem: Dict, arg: str) -> int:
-    if arg.isnumeric():
+    if "not" in arg:
+        if get_arg(mem, arg.replace("not ", "")):
+            return 1
+        else:
+            return 0
+    elif arg.isnumeric():
         return int(arg)
     else:
         return int(mem[arg])
+
 
 def make(mem: Dict, command: Command, *args) -> Any:
     if command == Command.MOV:
@@ -29,4 +37,4 @@ def make(mem: Dict, command: Command, *args) -> Any:
         if mem[resurses.FLAG] != 0:
             make(mem, Command.JMP, args[0])
     elif command == Command.PRINT:
-        print(mem[args[0]])
+        print(get_arg(mem, args[0]))
